f6ae726f5631c23d2da7fcd802aba213d4f7acad
Lauro Ware <lauro.ware@gmail.com>
2023-06-28 15:12:03 -0300

    baja de resolucion de imagenes

--- src/exports/imagenesDulce.js
+++ src/exports/imagenesDulce.js
@@ -1,11 +1,11 @@
-import D1 from "../assets/dulce/D1.png";
-import D2 from "../assets/dulce/D2.png";
-import D3 from "../assets/dulce/D3.png";
-import D4 from "../assets/dulce/D4.png";
-import D5 from "../assets/dulce/D5.png";
-import D6 from "../assets/dulce/D6.png";
-import D7 from "../assets/dulce/D7.png";
-import D8 from "../assets/dulce/D8.png";
-import D9 from "../assets/dulce/D9.png";
+import D1 from "../assets/dulce/D1.jpg";
+import D2 from "../assets/dulce/D2.jpg";
+import D3 from "../assets/dulce/D3.jpg";
+import D4 from "../assets/dulce/D4.jpg";
+import D5 from "../assets/dulce/D5.jpg";
+import D6 from "../assets/dulce/D6.jpg";
+import D7 from "../assets/dulce/D7.jpg";
+import D8 from "../assets/dulce/D8.jpg";
+import D9 from "../assets/dulce/D9.jpg";
 
 export default [D1, D2, D3, D4, D5, D6, D7, D8, D9];
--- src/exports/imagenesSalado.js
+++ src/exports/imagenesSalado.js
@@ -1,11 +1,11 @@
-import S1 from '../assets/salado/S1.png';
-import S2 from '../assets/salado/S2.png';
-import S3 from '../assets/salado/S3.png';
-import S4 from '../assets/salado/S4.png';
-import S5 from '../assets/salado/S5.png';
-import S6 from '../assets/salado/S6.png';
-import S7 from '../assets/salado/S7.png';
-import S8 from '../assets/salado/S8.png';
-import S9 from '../assets/salado/S9.png';
+import S1 from "../assets/salado/S1.jpg";
+import S2 from "../assets/salado/S2.jpg";
+import S3 from "../assets/salado/S3.jpg";
+import S4 from "../assets/salado/S4.jpg";
+import S5 from "../assets/salado/S5.jpg";
+import S6 from "../assets/salado/S6.jpg";
+import S7 from "../assets/salado/S7.jpg";
+import S8 from "../assets/salado/S8.jpg";
+import S9 from "../assets/salado/S9.jpg";
 
 export default [S1, S2, S3, S4, S5, S6, S7, S8, S9];
